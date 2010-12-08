d381979b60f68808363dd66064fb7b0999c455bc
Steven Steffen <ssteffen@siu.edu>
2010-12-08 14:26:40 -0600

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-#ActionController::Routing::Routes.draw do
+ActionController::Routing::Routes.draw do
   match 'forms/:id/:action/:field_id', :controller => "forms"
   match 'forms/:id/:action', :controller => "forms"
   
   resources :forms
-#end
+end
